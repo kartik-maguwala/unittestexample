615f971271560f02c1e19caf04538ea714e59f68
Kartik <kartik.maguwala@gmail.com>
2021-02-08 19:49:42 +0530

    First commit

---
+++ src/test/java/ch01/ScoreCollectionTest.java
@@ -0,0 +1,23 @@
+package ch01;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+public class ScoreCollectionTest {
+
+    @Test
+    public void answerArithmeticMeanOfTwoNumbers() {
+        // Arrange
+        ScoreCollection collection = new ScoreCollection();
+        collection.add(() -> 5);
+        collection.add(() -> 7);
+
+        // Act
+        int actualResult = collection.arithmeticMean();
+
+        // Assert
+        assertThat(actualResult, equalTo(6));
+    }
+}
