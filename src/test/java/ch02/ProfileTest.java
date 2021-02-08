370e2bd375be24d4a23da919243579d56594f98f
Kartik <kartik.maguwala@gmail.com>
2021-02-08 21:04:35 +0530

    ch02 commit

---
+++ src/test/java/ch02/ProfileTest.java
@@ -0,0 +1,42 @@
+package ch02;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class ProfileTest {
+    Profile profile;
+    Question question;
+    Criteria criteria;
+
+    @Before
+    public void create() {
+        profile = new Profile("Bull Hockey, Inc.");
+        question = new BooleanQuestion(1, "Got bonuses?");
+        criteria = new Criteria();
+    }
+
+    @Test
+    public void matchAnswersFalseWhenMustWatchCriteriaNotMet() {
+        profile.add(new Answer(question, Bool.FALSE));
+
+        criteria.add(
+                new Criterion(new Answer(question, Bool.TRUE), Weight.MustMatch));
+
+        boolean matches = profile.matches(criteria);
+        assertFalse(matches);
+    }
+
+    @Test
+    public void matchAnswersTrueForAnyDontCareCriteria() {
+        profile.add(new Answer(question, Bool.FALSE));
+
+        criteria.add(
+                new Criterion(new Answer(question, Bool.TRUE), Weight.DontCare));
+
+        boolean matches = profile.matches(criteria);
+        assertTrue(matches);
+    }
+}
